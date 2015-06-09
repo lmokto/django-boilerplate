309cba59539a3e3421ffdd800f2973e27099670e
Salvador Nicolas <snicoper@gmail.com>
2015-06-10 00:08:52 +0200

    Cambiado to-top de la pagina
    http://crunchify.com/how-to-add-smooth-scrolling-back-to-top-button-to-your-wordpress-blog/

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-    // Cerrar alerts de bootstrap
-    $(".close").click(function(){
-        $(".alert").hide();
+// Boton scroll hacia arriba pagina
+// http://crunchify.com/how-to-add-smooth-scrolling-back-to-top-button-to-your-wordpress-blog/
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.to-top').fadeIn(duration);
+        } else {
+            jQuery('.to-top').fadeOut(duration);
+        }
     });
 
-    // UItoTop-jQuery-Plugin
-    $().UItoTop({ easingType: 'easeOutQuart' });
+    jQuery('.to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
 });
