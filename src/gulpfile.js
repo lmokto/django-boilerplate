0185b4343e2c50b481acab643a91ae3384968506
Salvador Nicolas <snicoper@gmail.com>
2015-08-19 10:20:06 +0200

    Movido static/css a static/stylesheets

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -8,37 +8,33 @@ var gulp = require('gulp'),
 
 // Paths
 var paths = {
-    css_input: ['./static/source/css/_imports.styl'],
-    css_output: './static/dist/css',
-    js_input: ['./static/source/js/*.js'],
-    js_output: './static/dist/js'
+    stylesheets: ['./static/sources/stylesheets/imports.styl'],
+    cssOutput: './static/dist/css/',
+    scripts: ['./static/sources/js/*.js'],
+    jsOutput: './static/dist/js/'
 };
 
 // Stylus y CSS
-gulp.task('stylus', function() {
-
-    return gulp.src(paths.css_input)
+gulp.task('css:stylus', function() {
+    return gulp.src(paths.stylesheets)
         .pipe(stylus({'include css': true, 'compress': true}))
         .pipe(concat('main.min.css'))
-        .pipe(gulp.dest(paths.css_output));
-
+        .pipe(gulp.dest(paths.cssOutput));
 });
 
 // Javascript
 gulp.task('scripts', function() {
-
-    return gulp.src(paths.js_input)
+    return gulp.src(paths.scripts)
         .pipe(concat('main.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(paths.js_output));
-
+        .pipe(gulp.dest(paths.jsOutput));
 });
 
 // Watchs
 gulp.task('watch', function() {
-    gulp.watch('./static/source/css/*.*', ['stylus']);
+    gulp.watch('./static/source/stylesheets/**/*.*', ['css:stylus']);
     gulp.watch('./static/source/js/*.*', ['scripts']);
 });
 
 // Default
-gulp.task('default', ['stylus', 'scripts', 'watch']);
+gulp.task('default', ['css:stylus', 'scripts', 'watch']);
