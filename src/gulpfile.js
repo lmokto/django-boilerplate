d0cd757d09150b0ca853b2349059df5435134d8c
Salvador Nicolas <snicoper@gmail.com>
2015-08-20 17:46:46 +0200

    Movido stylesheets a styles

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,25 +6,17 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass');
 
-// Paths
-var paths = {
-    stylesheets: ['./static/sources/stylesheets/imports.scss'],
-    cssOutput: './static/dist/css/',
-    scripts: ['./static/sources/js/*.js'],
-    jsOutput: './static/dist/js/'
-};
-
-// Stylus y CSS
+// Sass
 gulp.task('css:sass', function() {
-    return gulp.src(paths.stylesheets)
+    return gulp.src(['static/sources/styles/imports.scss'])
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(concat('main.min.css'))
-        .pipe(gulp.dest(paths.cssOutput));
+        .pipe(gulp.dest('static/dist/css/'));
 });
 
 // Javascript
 gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
+    return gulp.src(['static/sources/js/**/*.js'])
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(paths.jsOutput));
@@ -32,8 +24,8 @@ gulp.task('scripts', function() {
 
 // Watchs
 gulp.task('watch', function() {
-    gulp.watch('./static/sources/stylesheets/**/*.*', ['css:sass']);
-    gulp.watch('./static/sources/js/**/*.*', ['scripts']);
+    gulp.watch('./static/sources/styles/**/*.*', ['css:sass']);
+    gulp.watch('static/sources/js/**/*.js', ['scripts']);
 });
 
 // Default
