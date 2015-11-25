5b8e656c5d33c4f204fe964796330cfb6904c894
Salvador Nicolas <snicoper@gmail.com>
2015-11-25 18:31:35 +0100

    Añadido url() para IndexView

--- src/apps/home/urls.py
+++ src/apps/home/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 app_name = 'home'
 
-urlpatterns = []
+urlpatterns = [
+    # /home/
+    url(
+        regex=r'^$',
+        view=views.IndexView.as_view(),
+        name='index'
+    ),
+]
+
