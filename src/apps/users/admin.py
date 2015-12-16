8b51ffbd1ee9c63d75601d2c621ec8ca68535ccc
Salvador Nicolas <snicoper@gmail.com>
2015-12-16 04:33:08 +0100

    Añadida primera migracion con makemigrations

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,23 +1,25 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm as AuthUserChangeForm
+from django.contrib.auth.forms import UserCreationForm as AuthUserCreationForm
+
 
 from .models import User
 
 
-class MyUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
+class UserChangeForm(AuthUserChangeForm):
+    class Meta(AuthUserChangeForm.Meta):
         model = User
 
 
-class MyUserCreationForm(UserCreationForm):
+class UserCreationForm(AuthUserCreationForm):
 
-    error_message = UserCreationForm.error_messages.update({
+    error_message = AuthUserCreationForm.error_messages.update({
         'duplicate_username': 'This username has already been taken.'
     })
 
-    class Meta(UserCreationForm.Meta):
+    class Meta(AuthUserCreationForm.Meta):
         model = User
 
     def clean_username(self):
@@ -31,8 +33,8 @@ class MyUserCreationForm(UserCreationForm):
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
-    form = MyUserChangeForm
-    add_form = MyUserCreationForm
+    form = UserChangeForm
+    add_form = UserCreationForm
     # fieldsets = AuthUserAdmin.fieldsets + (
     #    (None, {'fields': ('webpage', 'photo')}),
     # )
